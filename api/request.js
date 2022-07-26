8797b85ee40b86823a118a09cc61897a25236320
María Fernanda Monroy <maria.monroy@sofka.com.co>
2022-07-26 18:40:14 -0500

    modifing some code in request, and deadline-created data in database, and creating task card styles

--- api/request.js
+++ api/request.js
@@ -2,12 +2,13 @@ const API_URL =
   "https://my-json-server.typicode.com/mariafernandamonroy/M3U2TrelloApp-mafemonroy";
 
 axios
-  .get(`${API_URL}/task`)
+  .get(`${API_URL}/tasks`)
   .then((response) => showAllTask(response.data))
   .catch((error) => console.error(error));
 
 const showAllTask = (data) => {
   data.map((task) => createTask(task));
+  console.log(data)
 };
 
 const createTask = (task) => {
@@ -16,22 +17,23 @@ const createTask = (task) => {
 
   let taskTitle = document.createElement("h3");
   taskTitle.classList.add("card-task__title");
-  taskTitle.innerText = task.title;
+  taskTitle.innerText = `${task.title}`;
 
   let taskResponsible = document.createElement("p");
   taskResponsible.classList.add("card-task__responsible");
-  taskResponsible.innerText = `<span class= "card-task__responsible>Responsable:</span> ${task.person}`;
+  taskResponsible.innerHTML = `<span class= "card-task__responsible">Responsable:</span> ${task.person}`;
 
   let taskDetails = document.createElement("p");
   taskDetails.classList.add("card-task__details");
-  taskDetails.innerText = `<span class= "card-task__details>Descripción:</span> ${task.details}`;
+  taskDetails.innerHTML = `<span class= "card-task__details">Descripción:</span> ${task.details}`;
 
   let taskDate = document.createElement("p");
   taskDate.classList.add("card-task__date");
-  taskDate.innerText = `<span class= "card-task__date>Plazo:</span> ${dateFormat(
+  taskDate.innerHTML = `<span class= "card-task__date">Plazo:</span> ${dateFormat(
     task.deadline
   )}`;
-
+  
+  console.log(dateFormat(task.deadline))
   newTask.appendChild(taskTitle);
   newTask.appendChild(taskResponsible);
   newTask.appendChild(taskDetails);
@@ -41,13 +43,13 @@ const createTask = (task) => {
   let columnInProgress = document.querySelector("#inProgressTask");
   let columnDone = document.querySelector("#doneTasks");
 
-  if(task.state === "to-do") {
+  if (task.state === "to-do") {
     columnToDo.appendChild(newTask);
   }
-  if(task.state === "inProgress") {
+  if (task.state === "inProgress") {
     columnInProgress.appendChild(newTask);
   }
-  if(task.state === "done") {
+  if (task.state === "done") {
     columnDone.appendChild(newTask);
   }
 };
